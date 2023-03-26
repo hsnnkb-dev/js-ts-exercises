c8b597c44f0588116b18c0af35d5217d486baa06
hasan <hsnnkb.dev@gmail.com>
2023-03-26 22:25:36 +0100

    Add folder structure, Add variable contents

---
+++ basics/js-variables.js
@@ -0,0 +1,5 @@
+// Variables
+const one = 1; // Containerc contents that can't be changed
+let two; // Container contents can be changed
+var three; // Container contents can be changed, global scope, can lead to variable pollution
+
